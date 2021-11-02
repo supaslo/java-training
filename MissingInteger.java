21d1fa8b05c12692edad01e55689d721b909f37c
supaslo <supaslo222@gmail.com>
2021-11-02 00:12:06 -0500

    updating test to pass 100%

--- MissingInteger.java
+++ MissingInteger.java
@@ -1,6 +1,7 @@
 // you can also use imports, for example:
 // import java.util.*;
 import java.util.Arrays;
+import java.util.HashSet;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
@@ -10,9 +11,9 @@ class MissingInteger {
         int[] intArray1 = new int[]{1, 3, 6, 4, 1, 2}; 
         int[] intArray2 = new int[]{1,2,3}; 
         int[] intArray3 = new int[]{-1,-3};
-        System.out.println("Missing Number for Test 1 is:  " + findLowestNumberNotFound(intArray1));
-        System.out.println("Missing Number for Test 2 is:  " + findLowestNumberNotFound(intArray2));
-        System.out.println("Missing Number for Test 3 is:  " + findLowestNumberNotFound(intArray3));
+        System.out.println("Missing Number for Test 1 is:  " + findSmallestNumberNotIncluded(intArray1));
+        System.out.println("Missing Number for Test 2 is:  " + findSmallestNumberNotIncluded(intArray2));
+        System.out.println("Missing Number for Test 3 is:  " + findSmallestNumberNotIncluded(intArray3));
     }
     public static int findLowestNumberNotFound(int[] intArray) {
         Arrays.sort(intArray);
@@ -44,4 +45,22 @@ class MissingInteger {
             return nextNumber;
         }
     }
+    public static int findSmallestNumberNotIncluded(int[] intArray) {
+        int length = intArray.length;
+        int lowestNumber = 1;
+        HashSet<Integer> set = new HashSet<>();
+        for (int a : intArray) {
+            if (a > 0) {
+                set.add(a);
+            }
+        }
+        for (int i = 1; i <= length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            } else {
+                lowestNumber++;
+            }
+        }
+        return lowestNumber;
+    }    
 }
